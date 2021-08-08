9b9b3003cbf12ae06b9efba69728912ef048dfdd
wangxingkang <wang_xingkang@qq.com>
2021-08-08 19:48:28 +0800

    test: add isLernaPackage test

---
+++ src/isLernaPackage/index.test.ts
@@ -0,0 +1,16 @@
+import { join } from 'path';
+import { isLernaPackage } from '../index';
+
+const fixtures = join(__dirname, 'fixtures');
+
+describe('isLernaPackage', () => {
+  test('is lerna', () => {
+    expect(isLernaPackage(join(fixtures, 'is-lerna'))).toEqual(true);
+  });
+
+  test('is not lerna', () => {
+    expect(isLernaPackage(join(fixtures, 'is-not-lerna'))).toEqual(false);
+  });
+});
+
+
