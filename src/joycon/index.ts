722290c3bc5e71d4a9c3f01928c735e7de1f704f
wangxingkang <wang_xingkang@qq.com>
2021-08-10 17:38:07 +0800

    feat: replace joycon-ts-loader

--- src/joycon/index.ts
+++ src/joycon/index.ts
@@ -1,5 +1,5 @@
 import JoyCon from 'joycon';
-import JoyconTsLoader from 'joycon-ts-loader';
+import JoyconTsLoader from '@walrus/joycon-ts-loader';
 
 const joycon = new JoyCon();
 joycon.addLoader(JoyconTsLoader);
